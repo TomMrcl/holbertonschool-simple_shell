199d8dc046806b117d953cb20424159c9602dc5a
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2026-01-07 09:12:01 +0000

    réorganiser les déclarations de fonctions dans main.h pour une meilleure lisibilité

--- main.h
+++ main.h
@@ -16,20 +16,18 @@ char *read_command(void);
 /* execute */
 void execute_command(char *command, char *argv0, int cmd_number, char **envp);
 
-
 /* utils */
 int is_executable(char *path);
 void print_error(char *program_name, int cmd_number, char *command);
 char *trim_whitespace(char *str);
-char *get_env_value(char **envp, const char *name);
-char *find_command_in_path(char *command, char **envp);
-
 
 char **parse_command(char *command);
 void free_array(char **array);
 
+char *get_env_value(char **envp, const char *name);
+char *find_command_in_path(char *command, char **envp);
+
 /* PATH utils */
 char *get_path_env(void);
-char *find_command_in_path(char *command);
 
 #endif /* MAIN_H */
